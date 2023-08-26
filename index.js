205662127260f559eab5d3e05e4fb4e4044b22d3
ulrichnk <kapkapulrich@gmail.com>
2023-08-26 11:42:13 +0200

    p

--- index.js
+++ index.js
@@ -1,29 +1,78 @@
-const express = require('express');
-const jsonServer = require('json-server');
-const cors = require('cors');
-
+const express = require("express");
 const app = express();
-const router = jsonServer.router('db.json'); // Remplacez par le chemin vers votre fichier de données JSON
-const middlewares = jsonServer.defaults();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+const bodyParser = require("body-parser");
 
-app.use(cors()); // Utiliser le middleware cors
+app.use(bodyParser.json());
 
-// Configurer les en-têtes CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'react-inventaire.web.app'); // Remplacez * par vos domaines autorisés
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Méthodes HTTP autorisées
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // En-têtes autorisés
-  next();
+// curl -i https://some-app.cyclic.app/myFile.txt
+app.get("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  try {
+    let s3File = await s3
+      .getObject({
+        Bucket: process.env.BUCKET,
+        Key: filename,
+      })
+      .promise();
+
+    res.set("Content-type", s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
+  } catch (error) {
+    if (error.code === "NoSuchKey") {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
+    } else {
+      console.log(error);
+      res.sendStatus(500).end();
+    }
+  }
 });
 
-// Utiliser les middlewares de json-server
-app.use(middlewares);
+// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
+app.put("*", async (req, res) => {
+  let filename = req.path.slice(1);
 
-// Utiliser le routeur de json-server
-app.use(router);
+  console.log(typeof req.body);
 
-const port = 3000;
+  await s3
+    .putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
+app.delete("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  await s3
+    .deleteObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+// /////////////////////////////////////////////////////////////////////////////
+// Catch all handler for all other request.
+app.use("*", (req, res) => {
+  res.sendStatus(404).end();
+});
+
+// /////////////////////////////////////////////////////////////////////////////
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`index.js listening at http://localhost:${port}`);
 });
-
